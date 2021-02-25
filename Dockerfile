a7d3231170734b5980e1c3dec74a7b3d008a5832
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-02-25 14:51:44 +0300

    First commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM tiangolo/uvicorn-gunicorn:python3.8-slim
+
+RUN pip install --no-cache-dir fastapi[all] google-cloud-pubsub
+
+COPY ./app /app
---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class ItemViewStreamEvent(BaseModel):
+    itemId: Optional[str]
+    deviceId: Optional[str] 
+    ipAddress: Optional[str] 
+    timestamp: Optional[str]
+
+@app.post('/')
+async def main(event: ItemViewStreamEvent):
+    print(event)
+    return {'Message': 'Successful post'}
