0ffc68a2ef70a4ba998ecd80b29582711f3f0bae
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-02-26 16:58:09 +0300

    Added some pubsub magic

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from google.cloud import  pubsub_v1
+import os
+import json
 
 app = FastAPI()
+
+publisher = pubsub_v1.PublisherClient()
+project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+topic = os.getenv('TOPIC_NAME')
+topic_name = f'projects/{project_id}/topics/{topic}'
+
 
 class ItemViewStreamEvent(BaseModel):
     itemId: Optional[str]
@@ -13,5 +22,13 @@ class ItemViewStreamEvent(BaseModel):
 
 @app.post('/')
 async def main(event: ItemViewStreamEvent):
-    print(event)
-    return {'Message': 'Successful post'}
+    data = {
+        "viewId": event.viewId,
+        "deviceId" : event.deviceId,
+        "itemId": event.itemId,
+        "ipAddress": event.ipAddress,
+        "timestamp": event.timestamp,
+    }
+    data = json.dumps(data).encode('utf-8')
+    future = publisher.publish(topic_name, data)
+    return future.result()
