d0e5f1206af92512665bae3e69bd7c345af60fa0
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-03-08 13:46:31 +0300

    Added kafka producer

--- app/main.py
+++ app/main.py
@@ -5,12 +5,26 @@ from google.cloud import  pubsub_v1
 import os
 import json
 
+from confluent_kafka import Producer
+import socket
+
 app = FastAPI()
 
 publisher = pubsub_v1.PublisherClient()
 project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
 topic = os.getenv('TOPIC_NAME')
 topic_name = f'projects/{project_id}/topics/{topic}'
+
+conf = {
+        'bootstrap.servers': os.environ.get('SERVERS'),
+        'sasl.mechanisms': 'PLAIN',
+        'security.protocol': 'SASL_SSL',
+        'sasl.username': os.environ.get("USERNAME"),
+        'sasl.password': os.environ.get("PASSWORD"),
+        'auto.offset.reset': 'earliest',
+    }
+
+producer = Producer(conf)
 
 
 class ItemViewStreamEvent(BaseModel):
@@ -30,5 +44,6 @@ async def main(event: ItemViewStreamEvent, x_forwarded_for: Optional[str] = Head
     }
     data["ipAddress"] = str(x_forwarded_for)
     data = json.dumps(data).encode('utf-8')
+    producer.produce(topic, key=event.viewId, value=data)
     future = publisher.publish(topic_name, data)
     return future.result()
